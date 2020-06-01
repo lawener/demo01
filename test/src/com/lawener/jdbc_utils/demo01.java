f1c4c6f0b24d08ffbb5edc5f9c4d756c09174d12
lawener <lawenerlei@163.com>
2020-06-01 15:14:48 +0800

    miantest1

--- test/src/com/lawener/jdbc_utils/demo01.java
+++ test/src/com/lawener/jdbc_utils/demo01.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class demo01 {
     public static void main(String[] args) {
-        //测试git添加的注释
+        //测试git添加的注释123
         System.out.println("hello");
         Scanner scannerUsername= new Scanner(System.in);
         String next = scannerUsername.next();
